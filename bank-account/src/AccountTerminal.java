9865f448e17ef1f19632a15ff0f346fd514043dc
theyloac <83250591+theyloac@users.noreply.github.com>
2023-11-17 12:38:24 -0500

    Update AccountTerminal.java

--- bank-account/src/AccountTerminal.java
+++ bank-account/src/AccountTerminal.java
@@ -1,5 +1,35 @@
-public class BankTerminal {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+import java.util.Scanner;
+
+public class AccountTerminal {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        // Variables to store account information
+        int number;
+        String agency;
+        String clientName;
+        double balance;
+
+        // Getting account number from user
+        System.out.println("Please, enter the Account Number:");
+        number = scanner.nextInt(); // Reads an integer from the user
+        scanner.nextLine(); // Consumes the newline character after the integer input
+
+        // Getting agency from user
+        System.out.println("Please, enter the Agency:");
+        agency = scanner.nextLine(); // Reads a line of text (String)
+
+        // Getting client name from user
+        System.out.println("Please, enter the Client Name:");
+        clientName = scanner.nextLine(); // Reads a line of text (String)
+
+        // Getting balance from user
+        System.out.println("Please, enter the Balance:");
+        balance = scanner.nextDouble(); // Reads a double (decimal number)
+
+        // Displaying the account information
+        System.out.println("Hello " + clientName + ", thank you for creating an account in our bank, your agency is " + agency + ", account " + number + " and your balance " + balance + " is already available for withdrawal.");
+
+        scanner.close(); // Closing the scanner object
     }
 }
